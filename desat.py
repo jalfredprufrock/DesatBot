25c964ad009fdc31df5b542d51decc70c83c6d37
jalfredprufrock <g.chronis01@gmail.com>
2019-08-09 12:38:59 -0400

    Posting to imgur works
    The code can now post a desaturated  photo back to imgur. The image can be provided locally or from a url.

--- desat.py
+++ desat.py
@@ -1,6 +1,11 @@
 import math
 from PIL import Image
 import colorsys
+import requests
+from io import BytesIO
+import sys
+import base64
+import os
 
 def convertPixel(mult, rgb):
     r= rgb[0]
@@ -17,27 +22,63 @@ def convertPixel(mult, rgb):
     r,g,b= colorsys.hsv_to_rgb(h,s,v)
     return [r,g,b]
 
-def convertImage(image, mult):
-    im = Image.open(image)
-    out = Image.new('RGB', im.size, 0xffffff)
-    width, height = im.size
+def convertImage(url, mult, isLocal):
+    response = None
+    img = None
+    if not isLocal:
+        response = requests.get(url)
+        img = Image.open(BytesIO(response.content))
+    else:
+        im = Image.open(url)
+
+    out = Image.new('RGB', img.size, 0xffffff)
+    width, height = img.size
+
     for x in range(width):
         for y in range(height):
-            r,g,b = im.getpixel((x,y))
-            rgbNew =convertPixel(mult, [r,g,b])
+            r,g,b = img.getpixel((x,y))
+            rgbNew = convertPixel(mult, [r,g,b])
             rounded = [round(x) for x in rgbNew]
             out.putpixel((x,y), tuple(rounded))
-    print("done converting image")
-    out.save(image)
-    return
 
-def main(image, mult):
-    mult = None
-    if mult is None : mult= 50
-    if(math.isnan(mult)):
+    print("done converting image")
+    if not isLocal:
+        return handleImgurPOST(out)
+    else:
+        out.save(url)
+        return
+
+def handleImgurPOST(img):
+    client_id = os.environ.get('client_id')
+    if client_id is None:
+        print("client_id not found in env")
+        return None
+
+    postUrl = "https://api.imgur.com/3/image"
+    
+    headers = {"Authorization": "Client-ID "+client_id}
+
+    buffered = BytesIO()
+    img.save(buffered, format="JPEG")
+    img_str = base64.b64encode(buffered.getvalue())
+    resp = requests.post(postUrl, headers=headers, data = {'image':img_str})
+    
+
+    json = resp.json()
+
+    if 200 == int(json['status']):
+        return json['data']['link']
+    else:
+       print( 'error')
+
+def main(url, mult, isLocal = True):
+    if mult is None : mult= 75
+    if(math.isnan(int(mult))):
         print("multiplier not a number")
         return "error"
-    convertImage(image, mult)
+    if isLocal.lower() == "false":
+        isLocal = False
+    return convertImage(url, mult, isLocal)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1],sys.argv[2],sys.argv[3])
